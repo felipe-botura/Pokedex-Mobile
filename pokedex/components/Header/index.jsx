66ea1bed239dbf3ab9cc8b23cf7e59d88eaf3372
felipe-botura <felipebotura7@gmail.com>
2025-07-31 10:53:16 -0300

    Criação do projeto e parado em components/header/index.jsx

---
+++ pokedex/components/Header/index.jsx
@@ -0,0 +1,7 @@
+import { Image, Text, View, Pressable } from "react-native";
+import { styles } from "./styles";
+import { Ionicons }from "@expo/vector-icons"
+import { useRouter } from "expo-router"
+
+const imageSource = require("@/assets/images/header.jpg");
+
